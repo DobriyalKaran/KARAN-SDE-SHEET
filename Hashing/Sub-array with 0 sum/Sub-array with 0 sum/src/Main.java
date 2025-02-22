6ace5038c53a67f848584617600d1726e63c87fd
DobriyalKaran <KaranDobriyal007@gmail.com>
2025-02-22 11:17:42 +0530

    Code

---
+++ Hashing/Sub-array with 0 sum/Sub-array with 0 sum/src/Main.java
@@ -0,0 +1,57 @@
+/*
+Given an array of integers A, find and return whether the given array contains
+a non-empty subarray with a sum equal to 0.
+
+If the given array contains a sub-array with sum zero return 1, else return 0.
+
+Problem Constraints
+1 <= |A| <= 100000
+-10^9 <= A[i] <= 10^9
+
+ Input 1
+A = [1, 2, 3, 4, 5]
+o/p =  0
+
+Input 2
+A = [4, -1, 1]
+o/p = 1
+
+Explanation 1:
+    No subarray has sum 0.
+Explanation 2:
+    The subarray [-1, 1] has sum 0.
+*/
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+       int[] A = {4,-1,1};
+       int ans = solve(A);
+        System.out.println(ans);
+    }
+    public static int solve(int[] A) {
+        // Just write your code below to complete the function. Required Input is available to you as the function arguments.
+        // Do not print the result or any output. Just return the result via this function.
+        HashSet<Long> set = new HashSet<>();
+        long[] pf = new long[A.length];
+        pf[0] = A[0];
+        for(int i=1; i<A.length; i++)
+        {
+            pf[i] = A[i] + pf[i-1];
+        }
+        for(int i=0; i<pf.length; i++)
+        {
+            if(pf[i] == 0)
+            {
+                return 1;
+            }
+            if(set.contains(pf[i]))
+            {
+                return 1;
+            }
+            set.add(pf[i]);
+        }
+        return 0;
+    }
+}
